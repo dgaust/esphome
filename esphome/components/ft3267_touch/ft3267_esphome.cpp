dabefc0578ece94224ca5e1ea6ebe64787354b10
dgaust <dgaust@outlook.com>
2023-12-28 13:29:43 +1100

    Update ft3267_esphome.cpp

--- esphome/components/ft3267_touch/ft3267_esphome.cpp
+++ esphome/components/ft3267_touch/ft3267_esphome.cpp
@@ -28,14 +28,14 @@ TouchData FT3267Component::read_touch_data() {
 }
 
 bool FT3267Component::read_i2c(uint8_t reg, uint8_t *data, int len) {
-  // First, write the register address you want to read from
-  if (!this->write_to_i2c_device(reg, &reg, 1, true)) {
+  // Writing the register address we want to read from
+  if (!this->write_bytes(&reg, 1)) {
     ESP_LOGE("ft3267", "Failed to write to I2C device at register 0x%02X", reg);
     return false;
   }
 
-  // Then, read the data from the register
-  if (!this->read_from_i2c_device(reg, data, len, true)) {
+  // Reading the data from the register
+  if (!this->read_bytes(reg, data, len)) {
     ESP_LOGE("ft3267", "Failed to read from I2C device at register 0x%02X", reg);
     return false;
   }
