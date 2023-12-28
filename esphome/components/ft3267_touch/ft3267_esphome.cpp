30750ad6bfa4381218a6caf2345cc4ffef5ce0d5
dgaust <dgaust@outlook.com>
2023-12-28 13:22:37 +1100

    Update ft3267_esphome.cpp

--- esphome/components/ft3267_touch/ft3267_esphome.cpp
+++ esphome/components/ft3267_touch/ft3267_esphome.cpp
@@ -9,31 +9,39 @@ void FT3267Component::set_interrupt_pin(int pin) {
 }
 
 void FT3267Component::setup() {
-  // Setup code, including interrupt pin setup
+  if (this->interrupt_pin_ != -1) {
+    pinMode(this->interrupt_pin_, INPUT);
+    attachInterrupt(this->interrupt_pin_, std::bind(&FT3267Component::on_interrupt, this), FALLING);
+  }
+  // Additional setup code here, like initializing the sensor
 }
 
 int FT3267Component::get_touch_point_count() const {
-  // Implementation to retrieve the number of touch points
-  // This should return an integer representing the number of touch points detected
-  // Example (you need to replace this with actual logic to get touch points):
-  return touch_data.touch_count;
+  // Make sure this is updated somewhere with the actual touch point count
+  return this->touch_point_count_;
 }
 
 void FT3267Component::update() {
   TouchData data = this->read_touch_data();
-  // Process touch data
+  // Process touch data. For example:
+  this->touch_point_count_ = data.touch_count;
 }
 
 TouchData FT3267Component::read_touch_data() {
-  // Implementation to read touch data
+  TouchData data;
+  // Implement reading touch data from FT3267. For example:
+  // Read touch points from the sensor and populate 'data'
+  return data;
 }
 
 void FT3267Component::on_interrupt() {
-  // Interrupt handling
+  // Handle the interrupt. For example:
+  // Set a flag or read touch data directly
 }
 
 bool FT3267Component::read_i2c(uint8_t reg, uint8_t *data, int len) {
-  // I2C read implementation
+  // Implement I2C read functionality. For example:
+  return this->write_then_read(&reg, 1, data, len);
 }
 
 }  // namespace ft3267
