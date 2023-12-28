855a820dffb9982094eaeb1544f2f7dea62c037a
dgaust <dgaust@outlook.com>
2023-12-28 13:05:36 +1100

    Update touchscreen.py

--- esphome/components/ft3267_touch/touchscreen.py
+++ esphome/components/ft3267_touch/touchscreen.py
@@ -1,4 +1,5 @@
 import esphome.codegen as cg
+from esphome import pins
 import esphome.config_validation as cv
 from esphome.components import i2c
 from esphome.const import CONF_ID, CONF_UPDATE_INTERVAL
@@ -12,7 +13,7 @@ FT3267Component = ft3267_ns.class_('FT3267Component', cg.PollingComponent, i2c.I
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(FT3267Component),
     cv.Optional(CONF_UPDATE_INTERVAL): cv.update_interval,
-    cv.Optional(CONF_INTERRUPT_PIN): cv.gpio_pin,  # Define the interrupt pin
+    cv.Optional(CONF_INTERRUPT_PIN): pins.internal_gpio_input_pin_schema,  # Define the interrupt pin
     # ... other configurations ...
 }).extend(cv.COMPONENT_SCHEMA).extend(i2c.i2c_device_schema(0x38))
 
