e2e9d4d933a5c997c4bafd1b72f5b748e00787e0
dgaust <dgaust@outlook.com>
2023-12-31 14:58:00 +1100

    Update ft3267.cpp

--- esphome/components/ft3267_touchscreen/ft3267.cpp
+++ esphome/components/ft3267_touchscreen/ft3267.cpp
@@ -17,7 +17,7 @@ namespace ft3267 {
 
 static const char *const TAG = "ft3267Touchscreen";
 static const uint8_t FT3267_GESTURE_ID = 0x01;
-static const ft3267_gesture gestureData;
+ft3267_gesture gestureData;
 
 #define FT3267_ADDR                    (0x51)
 
