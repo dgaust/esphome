2c87d1b124b0c8a5968d893afa238ed21feb4373
dgaust <dgaust@outlook.com>
2024-08-23 17:26:42 +1000

    Changes to be committed: 	new file:   esphome/components/CHSC5816/CHSC5816Constants.h 	modified:   esphome/components/CHSC5816/__init__.py 	modified:   esphome/components/CHSC5816/chsc5816.cpp 	modified:   esphome/components/CHSC5816/chsc5816.h 	modified:   esphome/components/CHSC5816/touchscreen.py

--- esphome/components/CHSC5816/chsc5816.h
+++ esphome/components/CHSC5816/chsc5816.h
@@ -1,51 +1,27 @@
-/**************************************************************************/
-/*!
-  Author: Gustavo Ambrozio
-  Based on work by: Atsushi Sasaki (https://github.com/aselectroworks/Arduino-FT6336U)
-*/
-/**************************************************************************/
 
 #pragma once
 
 #include "esphome/components/i2c/i2c.h"
 #include "esphome/components/touchscreen/touchscreen.h"
 #include "esphome/core/component.h"
+#include "CHSC5816Constants.h"
 
 namespace esphome {
 namespace chsc5816 {
 
-using namespace touchscreen;
-
-static const uint8_t FT6X36_DEFAULT_THRESHOLD = 22;
-
-class chsc5816Touchscreen : public Touchscreen, public i2c::I2CDevice {
+class CHSC5816Touchscreen : public touchscreen::Touchscreen, public i2c::I2CDevice {
  public:
   void setup() override;
   void dump_config() override;
+  void loop() override;
 
-  void set_interrupt_pin(InternalGPIOPin *pin) { this->interrupt_pin_ = pin; }
-  void set_reset_pin(GPIOPin *pin) { this->reset_pin_ = pin; }
-  void set_threshold(uint8_t threshold) { this->threshold_ = threshold; }
+  void set_interrupt_pin(GPIOPin *pin) { this->interrupt_pin_ = pin; }
 
  protected:
-  void hard_reset_();
-  void update_touches() override;
+  GPIOPin *interrupt_pin_{nullptr};
 
-  InternalGPIOPin *interrupt_pin_{nullptr};
-  GPIOPin *reset_pin_{nullptr};
-  uint8_t threshold_{FT6X36_DEFAULT_THRESHOLD};
-
-  uint8_t read_touch_number_();
-
-  uint16_t read_touch_x_(uint8_t touch);
-  uint16_t read_touch_y_(uint8_t touch);
-  uint8_t read_touch_event_(uint8_t touch);
-  uint8_t read_touch_id_(uint8_t touch);
-  uint8_t read_touch_weight_(uint8_t touch);
-  uint8_t read_touch_misc_(uint8_t touch);
-
-  uint8_t read_byte_(uint8_t addr);
+  void read_touch_data();
 };
 
-}  // namespace ft63x6
+}  // namespace chsc5816
 }  // namespace esphome
--- esphome/components/CHSC5816/touchscreen.py
+++ esphome/components/CHSC5816/touchscreen.py
@@ -1,3 +1,4 @@
+
 import esphome.codegen as cg
 import esphome.config_validation as cv
 
@@ -5,41 +6,37 @@ from esphome import pins
 from esphome.components import i2c, touchscreen
 from esphome.const import CONF_ID, CONF_INTERRUPT_PIN, CONF_RESET_PIN, CONF_THRESHOLD
 
-CODEOWNERS = ["@gpambrozio"]
+CODEOWNERS = ["@dgaust"]
 DEPENDENCIES = ["i2c"]
 
-ft6336u_ns = cg.esphome_ns.namespace("ft63x6")
-FT63X6Touchscreen = ft6336u_ns.class_(
-    "FT63X6Touchscreen",
+chsc5816_ns = cg.esphome_ns.namespace("chsc5816")
+CHSC5816Touchscreen = chsc5816_ns.class_(
+    "CHSC5816Touchscreen",
     touchscreen.Touchscreen,
     i2c.I2CDevice,
 )
 
-CONF_FT63X6_ID = "ft63x6_id"
-
-
 CONFIG_SCHEMA = touchscreen.TOUCHSCREEN_SCHEMA.extend(
-    cv.Schema(
-        {
-            cv.GenerateID(): cv.declare_id(FT63X6Touchscreen),
-            cv.Optional(CONF_INTERRUPT_PIN): cv.All(
-                pins.internal_gpio_input_pin_schema
-            ),
-            cv.Optional(CONF_RESET_PIN): pins.gpio_output_pin_schema,
-            cv.Optional(CONF_THRESHOLD): cv.uint8_t,
-        }
-    ).extend(i2c.i2c_device_schema(0x38))
-)
-
+    {
+        cv.GenerateID(): cv.declare_id(CHSC5816Touchscreen),
+        cv.Optional(CONF_INTERRUPT_PIN): pins.internal_gpio_input_pin_schema,
+        cv.Optional(CONF_RESET_PIN): pins.gpio_output_pin_schema,
+        cv.Optional(CONF_THRESHOLD): cv.uint8_t,
+    }
+).extend(i2c.i2c_device_schema(0x48))  # Default I2C address for CHSC5816
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    await touchscreen.register_touchscreen(var, config)
     await i2c.register_i2c_device(var, config)
+    await touchscreen.register_touchscreen(var, config)
 
-    if interrupt_pin_config := config.get(CONF_INTERRUPT_PIN):
-        interrupt_pin = await cg.gpio_pin_expression(interrupt_pin_config)
-        cg.add(var.set_interrupt_pin(interrupt_pin))
-    if reset_pin_config := config.get(CONF_RESET_PIN):
-        reset_pin = await cg.gpio_pin_expression(reset_pin_config)
-        cg.add(var.set_reset_pin(reset_pin))
+    if interrupt_pin := config.get(CONF_INTERRUPT_PIN):
+        pin = await cg.gpio_pin_expression(interrupt_pin)
+        cg.add(var.set_interrupt_pin(pin))
+
+    if reset_pin := config.get(CONF_RESET_PIN):
+        pin = await cg.gpio_pin_expression(reset_pin)
+        cg.add(var.set_reset_pin(pin))
+
+    if CONF_THRESHOLD in config:
+        cg.add(var.set_threshold(config[CONF_THRESHOLD]))
